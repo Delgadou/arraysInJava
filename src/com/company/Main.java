595c3fc0b2ce9fcfa16e88a22eda4bb38da8a3cb
Henrique Oliveira <delgado.ohenrique@gmail.com>
2021-02-17 15:56:12 -0300

    Implementa ordenação de numero

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -6,24 +7,34 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int[]  myIntegers = getIntegers(3);
+        int sizeOfArray;
 
-        for(int i = 0; i<myIntegers.length; i++){
-            System.out.println("Element " + i + " has the value: " + myIntegers[i]);
+        System.out.print("Type the size of your array: ");
+        sizeOfArray = scanner.nextInt();
+
+        int[]  myIntegers = getIntegers(sizeOfArray);
+        System.out.println("Array without ordering");
+        printArray(myIntegers);
+
+        int[] sorted = descendingOrder(myIntegers);
+        System.out.println("Array with ordering");
+        printArray(sorted);
+
+        //System.out.println("The average is " + getAverage(myIntegers));
+    }
+
+    public static void printArray(int[] myArray){
+        for(int i = 0; i<myArray.length; i++){
+            System.out.println("[" + i + "]" + ": " + myArray[i]);
         }
-
-        System.out.println("The average is " + getAverage(myIntegers));
     }
 
     public static int[] getIntegers(int number){
-        System.out.println("Type " + number + " numbers");
-
+        System.out.print("Type " + number + " numbers: ");
         int[] values = new int[number];
-
         for(int i = 0; i<number; i++){
             values[i] = scanner.nextInt();
         }
-
         return values;
     }
 
@@ -34,6 +45,22 @@ public class Main {
         }
 
         return (double) sum / (double) values.length;
+    }
+
+    public static int[] descendingOrder(int[] array) {
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+
+        int temp;
+        for (int j = 0; j < sortedArray.length; j++) {
+            for (int i = 0; i < sortedArray.length - 1; i++) {
+                if (sortedArray[i] < sortedArray[i + 1]) {
+                    temp = sortedArray[i + 1];
+                    sortedArray[i + 1] = sortedArray[i];
+                    sortedArray[i] = temp;
+                }
+            }
+        }
+        return sortedArray;
     }
 }
 
